38d60bac9edd855eea1a8c50577237a5ae5ad349
David Alade <aladedavid4@gmail.com>
2025-04-05 23:15:16 +0100

    siteplan.html

--- Project/scripts/siteplan.js
+++ Project/scripts/siteplan.js
@@ -1,3 +1,30 @@
+// Select the hamburger menu and navigation links
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+// Add a click event listener to toggle the menu
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
+
+// target the current year id in the HTML
+const currentYear = document.getElementById('currentyear');
+
+
+// Getting the current year and date
+const today = new Date();
+
+// Add the current year to the HTML document
+currentYear.innerHTML = `${today.getFullYear()}`;
+
+
+let dateLastModified = document.lastModified;
+let displayContent = document.getElementById('lastmodified');
+
+
+displayContent.textContent = `Last Modification: ${dateLastModified}`;
+
 document.querySelectorAll('.learn-more-btn').forEach(button => {
     button.addEventListener('click', () => {
         alert('More information coming soon!');
